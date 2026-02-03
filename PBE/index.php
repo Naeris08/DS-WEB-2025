d61ab4821eb14f3f46e1380d71aa2a67d9c48276
Naeris08 <jgps.08042009@gmail.com>
2026-02-03 14:10:49 -0300

    Segundo commit 2026

---
+++ PBE/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aulasa</title>
+</head>
+<body>
+    <h1>Guia das aulas</a>
+    <a href="/POO/">Aulas de POO</a>
+</body>
+</html>
