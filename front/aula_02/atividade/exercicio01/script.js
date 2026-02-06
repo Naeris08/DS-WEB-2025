b5246bac41f17e5ae7e6ab4b125437d4a744cc9d
Naeris08 <jgps.08042009@gmail.com>
2026-02-06 15:23:59 -0300

    terceiro commit da aula 03

---
+++ front/aula_02/atividade/exercicio01/script.js
@@ -0,0 +1,5 @@
+//====================Adição do parágrafo====================
+function mostra(){
+    document.getElementById("conteudo").style.backgroundColor("#000000");
+    document.getElementById("conteudo").setAttribute();
+}
