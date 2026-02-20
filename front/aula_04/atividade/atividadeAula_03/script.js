fa542bd8e3a1a851ac5a521575bf9484c4c9a0e8
Naeris08 <jgps.08042009@gmail.com>
2026-02-20 10:46:46 -0300

    Atividade 3 certo

--- front/aula_04/atividade/atividadeAula_03/script.js
+++ front/aula_04/atividade/atividadeAula_03/script.js
@@ -1,23 +1,47 @@
-// Variável para contar o número de itens adicionados
-var newItem = 0;
-function addItem() {
- // Criando um novo item e incrementando o contador
-    newItem++;
+//* Criando o contador de Itens
+var item = 0
 
-// Criando um novo elemento de lista e preenchendo-o com os dados do formulário
-    let item = document.createElement("li");
-    let name = document.getElementById("nome");
-    let rm = document.getElementById("rm");
-    let telefone = document.getElementById("telefone");
-    let turma = document.getElementById("turma");
-    item.innerText = "Item " + newItem + " - Nome: " + name.value + ", RM: " + rm.value + ", Telefone: " + telefone.value + ", Turma: " + turma.value;    
-    // Adicionando o item à lista
-    document.getElementById("list").appendChild(item);
-// Atribuindo um ID único ao item para facilitar a remoção
-    item.setAttribute("id", newItem);
-// Criando um botão de remoção para cada item
-    let botaoRemover = document.createElement("button");
-    botaoRemover.innerText = "Remover";
-    botaoRemover.setAttribute("onclick", "removeItem(" + newItem + ")");
-    item.appendChild(botaoRemover);
+function add(){
+    //* Incrementando o contador de Itens
+    item ++
+
+    //* Crio o Item
+    let novoItem = document.createElement("li");
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let RM = document.getElementById("RM").value
+    let telefone = document.getElementById("telefone").value
+    let turma = document.getElementById("turma").value
+
+    //* Adiciono texto ao meu Item
+    novoItem.innerHTML = 
+    item + " - <br>" +
+    "Nome: " + nome + "<br>" +
+    "Email: " + email + "<br>" +
+    "RM: " + RM + "<br>" +
+    "Telefone: " + telefone + "<br>" +
+    "Turma: " + turma + "<br>";
+
+
+    //* Atribuo um ID
+    novoItem.setAttribute("id", item);
+
+    //* Cria o botão de remover
+    let botaoRemover = document.createElement("button")
+
+    //* Adiciona texto ao botão
+    botaoRemover.textContent = "Remover"
+
+    //* Adiciona uma função ao botão
+    botaoRemover.setAttribute("onclick",`remover(${item})`)
+
+    //* Adiciona o Botão ao novo item
+    novoItem.appendChild(botaoRemover);
+    document.getElementById("lista").appendChild(novoItem);
+}
+
+
+function remover(itemLista){
+    var item1 = document.getElementById(itemLista);
+    document.getElementById("lista").removeChild(item1);
 }
