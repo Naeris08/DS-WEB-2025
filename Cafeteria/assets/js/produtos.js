51c8bd936f448b150f47f4d2e232aa045f646d1d
Naeris08 <jgps.08042009@gmail.com>
2026-03-31 14:01:08 -0300

    Cafeteria commit 1

---
+++ Cafeteria/assets/js/produtos.js
@@ -0,0 +1,20 @@
+
+var divResposta = document.getElementById("resposta")
+
+// os inputs var inputNome   = document.getElementById("nome")
+
+document.addEventListener('DOMContentLoaded', getProdutos)
+document.getElementById('botaoEnviar').addEventListener('click', postProduto)
+
+async function getProdutos() {
+    
+}
+
+async function postProduto() {
+    
+}
+
+
+async function deleteProduto(id) {
+    
+}
